c2338656406f5391b4db94d4859243a0f79d27c5
KubranurGuzel <49498503+KubranurGuzel@users.noreply.github.com>
2019-07-02 19:40:17 +0300

    Add project files.

---
+++ ModernBusiness/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModernBusiness.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Portfolio1()
+        {
+            return View();
+        }
+        public ActionResult Portfolio2()
+        {
+            return View();
+        }
+        public ActionResult Portfolio3()
+        {
+            return View();
+        }
+        public ActionResult Portfolio4()
+        {
+            return View();
+        }
+        public ActionResult Portfolio42()
+        {
+            return View();
+        }
+        public ActionResult Portfolio43()
+        {
+            return View();
+        }
+        public ActionResult Portfolio5()
+        {
+            return View();
+        }
+        public ActionResult Blog1()
+        {
+            return View();
+        }
+        public ActionResult Blog2()
+        {
+            return View();
+        }
+        public ActionResult Blog3()
+        {
+            return View();
+        }
+        public ActionResult OtherPages1()
+        {
+            return View();
+        }
+        public ActionResult OtherPages2()
+        {
+            return View();
+        }
+        public ActionResult OtherPages3()
+        {
+            return View();
+        }
+        public ActionResult OtherPages4()
+        {
+            return View();
+        }
+        public ActionResult OtherPages5()
+        {
+            return View();
+        }
+    }
+}
---
+++ ModernBusiness/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModernBusiness.MvcApplication" Language="C#" %>
